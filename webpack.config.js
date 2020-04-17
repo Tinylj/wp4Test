b4907b1a960c12dc832415efbd0ff82a4ce08dd0
lujin <luj@lhcis.com>
2020-04-17 16:08:14 +0800

    [T4157]feature
    feature for vendors.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const webpack = require('webpack');
 const path = require('path');
 const config = {
-    entry: "./src/index.js",
+    mode: 'production',
+    entry: {
+        app: './src/index.js',
+        vendors:'./src/vendors.js',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'my-first-webpack.bundle.js'
+        filename: '[name].js'
     }
 };
 module.exports = config;
